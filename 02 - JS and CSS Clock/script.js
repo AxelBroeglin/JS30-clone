5754b80d5ab640d91c399e57f2d2fc6f3c690412
AxelBroeglin <axelbroeglin@gmail.com>
2022-07-10 00:28:08 +0100

    pseudo code

---
+++ 02 - JS and CSS Clock/script.js
@@ -0,0 +1,11 @@
+//need to catch hamds
+//need to apply rotation from center
+
+
+let today= new Date();
+ 
+console.log(today.getHours()); //Ouput: "12"
+console.log(today.getMinutes()); //Ouput: "28"
+console.log(today.getSeconds()); //Ouput: "57"
+
+//we have sec min and hours. Need to find formula to rotate hands.
