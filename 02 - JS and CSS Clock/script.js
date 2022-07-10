97d29f2729fc6ba1491a202624e8c2093f03713e
AxelBroeglin <axelbroeglin@gmail.com>
2022-07-10 16:08:28 +0100

    Work on getting hours

--- 02 - JS and CSS Clock/script.js
+++ 02 - JS and CSS Clock/script.js
@@ -1,4 +1,7 @@
 //need to catch hands
+const hands = document.querySelectorAll('.hand');
+
+//Catch each hand
 const hourHand = document.querySelector('.hour-hand');
 const minHand = document.querySelector('.min-hand');
 const secondHand = document.querySelector('.second-hand');
@@ -12,5 +15,10 @@ let today= new Date();
 console.log(today.getHours()); //Ouput: "12"
 console.log(today.getMinutes()); //Ouput: "28"
 console.log(today.getSeconds()); //Ouput: "57"
+
+
+
+hourHand.setAttribute('style', 'transform: rotate('today.getHours()'deg);');
+
 
 //we have sec min and hours. Need to find formula to rotate hands.
